7121aa271003d205aaa644b3b019f0df0bea6186
Sri Kavya <Kavyadev080496@gmail.com>
2024-02-05 14:15:17 -0500

    Nasa Api initial changes

---
+++ src/main/java/com/spring/nasaApi/SpringNASAAPI/Controller/NasaController.java
@@ -0,0 +1,63 @@
+package com.spring.nasaApi.SpringNASAAPI.Controller;
+
+import com.spring.nasaApi.SpringNASAAPI.Model.APODResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+public class NasaController {
+
+    @Value("DEMO_KEY")
+    private String apiKey;
+
+    private static final String APOD_URL = "https://api.nasa.gov/planetary/apod";
+
+
+
+    @GetMapping("/apod")
+    public List<APODResponse> getAPOD(@RequestParam(required = false) String date,
+                                @RequestParam(required = false) String start_date,
+                                @RequestParam(required = false) String end_date,
+                                @RequestParam(required = false) Integer count,
+                                @RequestParam(required = false) Boolean thumbs) {
+        RestTemplate restTemplate = new RestTemplate();
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(APOD_URL)
+                .queryParam("api_key", apiKey)
+                .queryParam("date", date)
+                .queryParam("start_date", start_date)
+                .queryParam("end_date", end_date)
+                .queryParam("count", count)
+                .queryParam("thumbs", thumbs);
+
+        String url = uriBuilder.toUriString();
+
+        try {
+            ResponseEntity<List<APODResponse>> responseEntity = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<APODResponse>>() {}
+            );
+            return responseEntity.getBody();
+        } catch (Exception e) {
+
+            try {
+                APODResponse singleResponse = restTemplate.getForObject(url, APODResponse.class);
+                return Collections.singletonList(singleResponse);
+            } catch (Exception ex) {
+
+                throw new RuntimeException( ex.getMessage(), ex);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/spring/nasaApi/SpringNASAAPI/SpringNasaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.nasaApi.SpringNASAAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNasaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
